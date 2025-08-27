156e5ea6b0b3d67c89e9e23856196b3115e36e55
Lautap1578 <lautaroporta9@gmail.com>
2025-08-27 12:05:33 -0300

    Use environment variables for Django settings

--- gym/settings.py
+++ gym/settings.py
@@ -3,11 +3,11 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-1234567890-esto-es-un-ejemplo'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "django-insecure-1234567890-esto-es-un-ejemplo")
 
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
